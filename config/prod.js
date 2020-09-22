d9a752b22d0e3c5e5a40cdf624210a253d994e36
선원진 <ctangtangbbang@gmail.com>
2020-09-22 22:01:00 +0900

    비밀 정보 보호

---
+++ config/prod.js
@@ -0,0 +1,5 @@
+const { mongoURI } = require("./key");
+
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+};
--- index.js
+++ index.js
@@ -2,22 +2,42 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const { User } = require("./model/User");
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
+
 mongoose
-  .connect(
-    "mongodb+srv://radi:1234@cluster0.e2e22.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
-  .then(() => console.log(`✅ Mongodb connect ..`))
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log(`✅ Mongodb connect Success! 🎉 `))
   .catch((err) => console.error(`❌ ${err}`));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Worldsdfsdf22!");
+});
+
+app.post("/register", (req, res) => {
+  //회원가입 할때 필요한 정보들을 Client에서 가져오면
+  //그것들을 Db에 넣어준다.
+
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
